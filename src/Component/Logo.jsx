63d1c2218bad9f04c5cafd289e05970c1347afa9
maumau <134153261+Maui0826@users.noreply.github.com>
2025-03-28 11:20:50 +0800

    3rd commit

--- src/Component/Logo.jsx
+++ src/Component/Logo.jsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <div className="navbar">
-      <img src="./assets/Logo.png" alt="LOGO" />
+      <img src="../../assets/Logo.png" alt="LOGO" />
       <p>Valenzuela National High School</p>
     </div>
   );
